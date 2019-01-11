9c870faffafe71afd05a73b8115eeecb16afde87
wang-tf <ternencewang@126.com>
2019-01-11 15:25:04 +0800

    v0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='pascal_voc_tools',
-    version='0.1.1',
+    version='0.1.2',
     author='ternence wang',
     author_email='ternencewang2015@outlook.com',
     description='some tools about pascal voc format dataset',
