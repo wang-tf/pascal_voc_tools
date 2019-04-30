5ff2dbb559908edb0acbd35c8a733aed60c3c685
wang-tf <ternencewang@126.com>
2019-04-30 16:46:25 +0800

    add description

--- pascal_voc_tools/__init__.py
+++ pascal_voc_tools/__init__.py
@@ -1,3 +1,23 @@
+"""
+Pascal Voc Tools
+===============
+
+This package provide some tools using for 
+pascal voc format dataset and some usrful
+functions.
+
+some function in the below list:
+1. XmlReader
+2. XmlWriter
+3. DatasetResize
+4. DataSplit
+5. DarknetDataset
+6. SplitImageAnnotation
+7. AnnotationTools
+8. AnchorsKMeans
+9. DarknetConfig
+"""
+
 from ._version import version
 from .xmlreader import XmlReader
 from .xmlwriter import XmlWriter
@@ -9,6 +29,9 @@ from .annotation_tools import AnnotationTools
 from .anchors_kmeans import AnchorsKMeans
 from .darknet_config import DarknetConfig
 
+__all__ = ['version', 'XmlReader', 'XmlWriter', 'DatasetResize',
+           'DataSplit', 'DarknetDataset', 'SplitImageAnnotation',
+           'AnnotationTools', 'AnchorsKMeans', 'DarknetConfig']
 
 name = 'pascal_voc_tools'
 __version__ = version
