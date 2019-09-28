07a575ddf8ef60a01e90e81ec469c5fba7001fe5
wang-tf <wang-tf@github.com>
2019-09-28 12:14:56 +0800

    using pbr

--- setup.py
+++ setup.py
@@ -1,26 +1,5 @@
-
 import setuptools
-from pascal_voc_tools._version import version
 
 
-with open('README.md', 'r') as fh:
-    long_description = fh.read()
+setuptools.setup(setup_requires=['pbr'], pbr=True)
 
-setuptools.setup(
-    name='pascal_voc_tools',
-    version=version,
-    author='ternence wang',
-    author_email='ternencewang2015@outlook.com',
-    description='some tools about pascal voc format dataset',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url='https://github.com/wang-tf/pascal_voc_tools',
-    packages=setuptools.find_packages(),
-    include_package_data=True,
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
-    install_requires=['jinja2', 'opencv-python', 'tqdm'],
-)
