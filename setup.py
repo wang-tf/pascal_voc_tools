999938c797f6dd71670610d002b2b1dfeb8b88a2
wang-tf <wang-tf@github.com>
2021-01-21 19:38:41 +0800

    format

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
-import setuptools
+#!/usr/bin/env python
 
+from setuptools import setup
 
-setuptools.setup(setup_requires=['pbr'], pbr=True)
-
+setup(setup_requires=['pbr>=1.9', 'setuptools>=17.1'], pbr=True)
